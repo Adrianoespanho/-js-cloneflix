39e92baa1a64bc8552817a6422591eb33e99d496
Adrianoespanho <112988600+Adrianoespanho@users.noreply.github.com>
2022-10-25 22:28:47 -0300

    javascript aula 25/10

---
+++ app.js
@@ -0,0 +1,17 @@
+let filme01 = 'imagens/filme01.jpg'
+let filme02 = 'imagens/filme02.jpg'
+let filme03 = 'imagens/filme03.jpg'
+let filme04 = 'imagens/filme04.jpg'
+let filme05 = 'imagens/filme05.jpg'
+let filme06 = 'imagens/filme06.jpg'
+document.querySelector(".container").innerHTML += "<img src= '"  + filme01 + " '>"
+document.querySelector(".container").innerHTML += "<img src= '"  + filme02 + " '>"
+document.querySelector(".container").innerHTML += "<img src= '"  + filme03 + " '>"
+document.querySelector(".container").innerHTML += "<img src= '"  + filme04 + " '>"
+document.querySelector(".container").innerHTML += "<img src= '"  + filme05 + " '>"
+document.querySelector(".container").innerHTML += "<img src= '"  + filme06 + " '>"
+
+
+//1 - acrescentar mais 3 filmes 
+//2 - personalizar o layout (trocar filmes, fundo, cores , fonts)
+//3 - armazenar os filmes em um unico vetor (array)
